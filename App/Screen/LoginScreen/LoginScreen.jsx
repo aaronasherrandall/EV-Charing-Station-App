0294744c46da58edf5cd17a88b539c5efaa6a768
Aaron Randall <aaronasherrandall@gmail.com>
2024-05-30 11:08:39 +0900

    added onPress event handler prop

--- App/Screen/LoginScreen/LoginScreen.jsx
+++ App/Screen/LoginScreen/LoginScreen.jsx
@@ -1,5 +1,6 @@
-import { View, Text, Image, StyleSheet } from 'react-native'
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
 import React from 'react'
+import Colors from '../../Utils/Colors'
 
 export default function LoginScreen() {
   return (
@@ -8,7 +9,7 @@ export default function LoginScreen() {
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center',
-      marginTop: 50,
+      marginTop: 80,
 
     }}>
       <Image source={require('./../../../assets/images/logo.png')}
@@ -19,6 +20,19 @@ export default function LoginScreen() {
         //external styling
         style={styles.bgImage}
       />
+      <View style={{padding:20}}>
+        <Text style={styles.heading}>Your ultimate EV charging Station Finder App</Text>
+        <Text style={styles.description}>Find an EV charger Station near you, plan your trip and so much more.</Text>
+      </View>
+      <TouchableOpacity style={styles.button}
+      onPress={()=>console.log("Button Clicked")}>
+        <Text style={{
+          color:Colors.WHITE,
+          textAlign: 'center',
+          fontFamily: 'outfit',
+          fontSize: 17,
+        }}>Login with Google</Text>
+      </TouchableOpacity>
     </View>
   )
 }
@@ -35,4 +49,24 @@ const styles = StyleSheet.create({
     marginTop: 20,
     objectFit: 'cover'
   },
+  heading:{
+    fontSize: 25,
+    fontFamily: 'outfit-bold',
+    textAlign: 'center',
+    marginTop: 20
+  },
+  description:{
+    fontSize: 17,
+    fontFamily: "outfit",
+    marginTop: 15,
+    textAlign: 'center',
+    color: Colors.GRAY,
+  },
+  button:{
+    backgroundColor: Colors.PRIMARY,
+    padding: 16,
+    display: 'flex',
+    borderRadius: 99,
+    marginTop: 40,
+  }
 })
